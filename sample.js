c2f35d5d24d15c7f535b83a529a3d870bedc4fb1
VulCoder28 <davidsiaw@palm.edu.gh>
2023-09-20 03:06:27 -0700

    'First-take'

---
+++ sample.js
@@ -0,0 +1,19 @@
+// var array=document.getElementsByTagName('input');
+// var fname=array[0].value;
+// var age=array[1].value;
+// if(age>18){
+//     alert(fname+" "+"is eligible to view the site");
+// }
+// else{
+//     alert(fname+" "+"is not eligible to view the site");
+
+// const text=document.querySelector('div');
+// text.addEventListener('click',test)
+// function test(){
+//     alert('Hello');
+// }
+// var test=document.getElementById('new');
+// test.addEventListener('click',sample);
+// function sample(){
+// alert('Hello');
+// };
---
+++ test.js
@@ -0,0 +1,37 @@
+// // var fname=prompt("Enter name: ");
+// // alert("Hello"+" "+fname);
+// // var age=prompt("Enter your age");
+// // if(age<18){
+// //     alert("You are not allowed to view site");
+// // }
+// // else{
+// //     alert("You are allowed to view site");
+// // }
+// var list=['Salma','Eugene','Justice'];
+// // console.log(list[0]);
+// // for(var x=0; x<=2;x++){
+// //  list[x];
+// // alert(list[x]);
+// // }
+// // var x=0;
+// // while(x<=2){
+
+// // alert(list[x]);
+// // x++;
+// // }
+// var x=0;
+// do{
+   
+    
+//     alert(list[x]);
+//     x++;
+
+// }while(x<=2);
+
+
+
+
+
+
+
+
